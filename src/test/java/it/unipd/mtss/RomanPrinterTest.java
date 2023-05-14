a780ba44796fee2d24dc74b9815b53336da2f50a
CiprianAgafitei <ciprianaga@gmail.com>
2023-05-14 15:47:02 +0200

    Test convert senza implementazione

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,5 +1,23 @@
 package it.unipd.mtss;
 
-public class RomanPrinterTest {
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.junit.Assert.assertEquals;;
 
+public class RomanPrinterTest {
+    @Test
+    public void testConvertZero() {
+        // Arrange
+        try (MockedStatic<IntegerToRoman> utilities = Mockito.mockStatic(IntegerToRoman.class)) {
+
+            utilities.when(() -> IntegerToRoman.convert(0)).thenReturn("");
+
+            // Act
+            String expected = "";
+
+            // Assert
+            assertEquals(expected, RomanPrinter.print(0));
+        }
+    }
 }

c4da5d0174cfc3a5fe69d1c9a23efb808dffbb23
Ivan Kayn <ionel.cainareanu1@gmail.com>
2023-05-14 15:25:28 +0200

    created unit tests for all cases (from 0 to 1000)

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -29,4 +29,109 @@ public class IntegerToRomanTest {
         // Assert
         assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
     }
+
+    @Test
+    public void testConvertFive() {
+        // Arrange
+        int numeroArabo = 5;
+        String numeroRomanoAtteso = "V";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertTen() {
+        // Arrange
+        int numeroArabo = 10;
+        String numeroRomanoAtteso = "X";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertFourteen() {
+        // Arrange
+        int numeroArabo = 14;
+        String numeroRomanoAtteso = "XIV";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertFifty() {
+        // Arrange
+        int numeroArabo = 50;
+        String numeroRomanoAtteso = "L";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertNinety() {
+        // Arrange
+        int numeroArabo = 90;
+        String numeroRomanoAtteso = "XC";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertHundred() {
+        // Arrange
+        int numeroArabo = 100;
+        String numeroRomanoAtteso = "C";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertNineHundred() {
+        // Arrange
+        int numeroArabo = 900;
+        String numeroRomanoAtteso = "CM";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
+    @Test
+    public void testConvertThousand() {
+        // Arrange
+        int numeroArabo = 1000;
+        String numeroRomanoAtteso = "M";
+
+        // Act
+        String numeroRomanoAttuale = IntegerToRoman.convert(numeroArabo);
+
+        // Assert
+        assertEquals(numeroRomanoAtteso, numeroRomanoAttuale);
+    }
+
 }
